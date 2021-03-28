0e9a6a1ba96e781f7e12d6cbcf8e7d5099668cc9
MensExperience <clannad.mun@gmail.com>
2021-03-28 18:19:55 +0900

    firstcommit

---
+++ 01.fizzbuzz/fizzBuzz.rb
@@ -0,0 +1,15 @@
+flg = 20
+i = 1
+while flg >= i
+    case
+    when i%15 == 0
+        puts "FizzBunzz"
+    when i%5 == 0
+        puts "Buzz"
+    when i%3 == 0
+        puts"Fizz"
+    else
+        puts i
+    end
+    i += 1
+end
