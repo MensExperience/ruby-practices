3cbf5a86d026673e2d67f03a871c272a4d1d50c1
MensExperience <clannad.mun@gmail.com>
2021-04-02 12:52:45 +0900

    fix: Completely fixed. Following the maedana's review.

--- 01.fizzbuzz/fizzBuzz.rb
+++ 01.fizzbuzz/fizzBuzz.rb
@@ -1,18 +1,10 @@
 #!/usr/bin/env ruby
 
-flg = 20
-i = 1
-while flg >= i
-    case
-    when i%15 == 0
-        puts "FizzBuzz"
-    when i%5 == 0
-        puts "Buzz"
-    when i%3 == 0
-        puts"Fizz"
-    else
-        puts i
-    end
-    i += 1
+(1..20).each do |i|
+  if i % 15 == 0
+    puts "FizzBuzz"    
+  elseif i % 5 == 0
+    puts "Buzz"
+  elseif i % 3 == 0
+    puts "Fizz"
 end
-
