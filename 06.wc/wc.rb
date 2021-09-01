196787ffaa171227888ed5c471c31e11a07ea2e5
shugowork <schw4one1win@gmail.com>
2021-09-01 16:36:04 +0900

    Implement class CalcFileInfo and display methods

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,7 +8,7 @@ class Main
     opt = ARGV.getopts('l')
     read_files = ARGV
     if read_files.empty?
-      StanderdInput.new.standard_input(opt)
+      StanderdInput.new.disp_standard_input(opt)
     else
       FileArguments.new.disp(read_files, opt)
     end
@@ -16,10 +16,14 @@ class Main
 end
 
 class FileArguments
+  def adjust_display(info)
+    info.to_s.rjust(7)
+  end
+
   def disp_total(total_line, total_word, total_bytesize, opt)
-    print " #{total_line.to_s.rjust(7)}"
-    print " #{total_word.to_s.rjust(7)}" unless opt['l']
-    print " #{total_bytesize.to_s.rjust(7)}" unless opt['l']
+    print " #{adjust_display(total_line)}"
+    print " #{adjust_display(total_word)}" unless opt['l']
+    print " #{adjust_display(total_bytesize)}" unless opt['l']
     puts ' total'
   end
 
@@ -27,14 +31,15 @@ class FileArguments
     total_line = 0
     total_word = 0
     total_bytesize = 0
+    file_arguments = CalcFileInfo.new
     read_files.each do |file|
       read_file = File.read(file)
-      print " #{read_file.split(/\R/).size.to_s.rjust(7)}"
-      print " #{read_file.split(/\s+/).size.to_s.rjust(7)}" unless opt['l']
-      print " #{read_file.bytesize.to_s.rjust(7)}" unless opt['l']
+      print " #{file_arguments.count_word(read_file)}"
+      print " #{file_arguments.count_line(read_file)}" unless opt['l']
+      print " #{file_arguments.count_bytesize(read_file)}" unless opt['l']
       puts " #{file}"
-      total_line += read_file.split(/\R/).size
-      total_word += read_file.split(/\s+/).size unless opt['l']
+      total_line += file_arguments.calc_word(read_file)
+      total_word += file_arguments.calc_line(read_file) unless opt['l']
       total_bytesize += read_file.bytesize unless opt['l']
     end
     disp_total(total_line, total_word, total_bytesize, opt) if read_files.size > 1
@@ -42,12 +47,35 @@ class FileArguments
 end
 
 class StanderdInput
-  def standard_input(opt)
-    input = $stdin.read
-    print " #{input.split(/\R/).size.to_s.rjust(7)}"
-    print " #{input.split(/\s+/).size.to_s.rjust(7)}" unless opt['l']
-    print " #{input.bytesize.to_s.rjust(7)}" unless opt['l']
+  def disp_standard_input(opt)
+    read_file = $stdin.read
+    standerd_input = CalcFileInfo.new
+    print " #{standerd_input.count_word(read_file)}"
+    print " #{standerd_input.count_line(read_file)}" unless opt['l']
+    print " #{standerd_input.count_bytesize(read_file)}" unless opt['l']
     puts
+  end
+end
+
+class CalcFileInfo
+  def count_word(read_file)
+    read_file.split(/\R/).size.to_s.rjust(7)
+  end
+
+  def count_line(read_file)
+    read_file.split(/\s+/).size.to_s.rjust(7)
+  end
+
+  def count_bytesize(read_file)
+    read_file.bytesize.to_s.rjust(7)
+  end
+
+  def calc_word(read_file)
+    read_file.split(/\R/).size
+  end
+
+  def calc_line(read_file)
+    read_file.split(/\s+/).size
   end
 end
 
