ddbae713ac7cd3c0cd9d7002f800eb3599f0fba8
shugowork <schw4one1win@gmail.com>
2021-09-01 15:28:15 +0900

    refactoring: Implement decisions in opt.

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -8,66 +8,46 @@ class Main
     opt = ARGV.getopts('l')
     read_files = ARGV
     if read_files.empty?
-      opt['l'] ? StanderdInput.new.standard_input_l : StanderdInput.new.standard_input
+      StanderdInput.new.standard_input(opt)
     else
-      opt['l'] ? FileArguments.new.disp_l(read_files) : FileArguments.new.disp(read_files)
+      FileArguments.new.disp(read_files, opt)
     end
   end
 end
 
 class FileArguments
-  def disp_total(total_line, total_word, total_bytesize)
+  def disp_total(total_line, total_word, total_bytesize, opt)
     print " #{total_line.to_s.rjust(7)}"
-    print " #{total_word.to_s.rjust(7)}"
-    print " #{total_bytesize.to_s.rjust(7)}"
+    print " #{total_word.to_s.rjust(7)}" unless opt['l']
+    print " #{total_bytesize.to_s.rjust(7)}" unless opt['l']
     puts ' total'
   end
 
-  def disp_total_l(total_line)
-    print " #{total_line.to_s.rjust(7)}"
-    puts ' total'
-  end
-
-  def disp(read_files)
+  def disp(read_files, opt)
     total_line = 0
     total_word = 0
     total_bytesize = 0
     read_files.each do |file|
       read_file = File.read(file)
       print " #{read_file.split(/\R/).size.to_s.rjust(7)}"
-      print " #{read_file.split(/\s+/).size.to_s.rjust(7)}"
-      print " #{read_file.bytesize.to_s.rjust(7)}"
+      print " #{read_file.split(/\s+/).size.to_s.rjust(7)}" unless opt['l']
+      print " #{read_file.bytesize.to_s.rjust(7)}" unless opt['l']
       puts " #{file}"
       total_line += read_file.split(/\R/).size
-      total_word += read_file.split(/\s+/).size
-      total_bytesize += read_file.bytesize
+      total_word += read_file.split(/\s+/).size unless opt['l']
+      total_bytesize += read_file.bytesize unless opt['l']
     end
-    disp_total(total_line, total_word, total_bytesize) if read_files.size > 1
-  end
-
-  def disp_l(read_files)
-    total_line = 0
-    read_files.each do |file|
-      read_file = File.read(file)
-      print " #{read_file.split(/\R/).size.to_s.rjust(7)}"
-      puts " #{file}"
-      total_line += read_file.lines.count
-    end
-    disp_total_l(total_line) if read_files.size > 1
+    disp_total(total_line, total_word, total_bytesize, opt) if read_files.size > 1
   end
 end
 
 class StanderdInput
-  def standard_input
+  def standard_input(opt)
     input = $stdin.read
     print " #{input.split(/\R/).size.to_s.rjust(7)}"
-    print " #{input.split(/\s+/).size.to_s.rjust(7)}"
-    puts " #{input.bytesize.to_s.rjust(7)}"
-  end
-
-  def standard_input_l
-    input = $stdin.read
-    puts " #{input.split(/\R/).size.to_s.rjust(7)}"
+    print " #{input.split(/\s+/).size.to_s.rjust(7)}" unless opt['l']
+    print " #{input.bytesize.to_s.rjust(7)}" unless opt['l']
+    puts
   end
 end
 
