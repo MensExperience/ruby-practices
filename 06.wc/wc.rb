054d63c467103aab33c1c542fb020d46edfd732d
shugowork <schw4one1win@gmail.com>
2021-09-08 22:14:16 +0900

    Importing reviews by JunichiIto
    Consolidate to Main class.Overall revision

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -6,80 +6,66 @@ require 'optparse'
 class Main
   def main
     opt = ARGV.getopts('l')
-    read_files = ARGV
-    if read_files.empty?
-      StanderdInput.new.disp_standard_input(opt)
-    else
-      FileArguments.new.disp(read_files, opt)
-    end
-  end
-end
-
-class FileArguments
-  def adjust_display(info)
-    info.to_s.rjust(7)
+    file_paths = ARGV
+    display(file_paths, opt)
   end
 
-  def disp_total(total_line, total_word, total_bytesize, opt)
-    print " #{adjust_display(total_line)}"
-    print " #{adjust_display(total_word)}" unless opt['l']
-    print " #{adjust_display(total_bytesize)}" unless opt['l']
-    puts ' total'
-  end
-
-  def disp(read_files, opt)
+  def display(file_paths, opt)
     total_line = 0
     total_word = 0
     total_bytesize = 0
-    file_arguments = CalcFileInfo.new
-    read_files.each do |file|
-      read_file = File.read(file)
-      print " #{file_arguments.count_word(read_file)}"
-      print " #{file_arguments.count_line(read_file)}" unless opt['l']
-      print " #{file_arguments.count_bytesize(read_file)}" unless opt['l']
-      puts " #{file}"
-      total_line += file_arguments.calc_word(read_file)
-      total_word += file_arguments.calc_line(read_file) unless opt['l']
-      total_bytesize += file_arguments.calc_bytesize(read_file) unless opt['l']
+    if file_paths.empty?
+      file_text = $stdin.read
+      display_detailed_information(file_text, opt)
+      puts
+    else
+      file_paths.each do |file_path|
+        file_text = File.read(file_path)
+        display_detailed_information(file_text, opt)
+        puts " #{adjust_display_position(file_path)}"
+      end
+      total_line += count_line(file_text)
+      unless opt['l']
+        total_word += count_word(file_text)
+        total_bytesize += count_bytesize(file_text)
+      end
+      display_total_count(total_line, total_word, total_bytesize, opt) if file_paths.size > 1
     end
-    disp_total(total_line, total_word, total_bytesize, opt) if read_files.size > 1
-  end
-end
-
-class StanderdInput
-  def disp_standard_input(opt)
-    read_file = $stdin.read
-    standerd_input = CalcFileInfo.new
-    print " #{standerd_input.count_word(read_file)}"
-    print " #{standerd_input.count_line(read_file)}" unless opt['l']
-    print " #{standerd_input.count_bytesize(read_file)}" unless opt['l']
-    puts
-  end
-end
-
-class CalcFileInfo
-  def count_word(read_file)
-    read_file.split(/\R/).size.to_s.rjust(7)
   end
 
-  def count_line(read_file)
-    read_file.split(/\s+/).size.to_s.rjust(7)
+  private
+
+  def display_detailed_information(file_text, opt)
+    print " #{adjust_display_position(count_line(file_text))}"
+      unless opt['l']
+        print " #{adjust_display_position(count_word(file_text))}"
+        print " #{adjust_display_position(count_bytesize(file_text))}"
+      end
   end
 
-  def count_bytesize(read_file)
-    read_file.bytesize.to_s.rjust(7)
+  def display_total_count(total_line, total_word, total_bytesize, opt)
+    print " #{adjust_display_position(total_line)}"
+    unless opt['l']
+      print " #{adjust_display_position(total_word)}"
+      print " #{adjust_display_position(total_bytesize)}"
+    end
+    puts ' total'
   end
 
-  def calc_word(read_file)
-    read_file.split(/\R/).size
+  def adjust_display_position(info)
+    info.to_s.rjust(7)
   end
 
-  def calc_line(read_file)
-    read_file.split(/\s+/).size
+  def count_line(file_text)
+    file_text.split(/\R/).size
   end
 
-  def calc_bytesize(read_file)
-    read_file.bytesize
+  def count_word(file_text)
+    file_text.split(/\s+/).size
+  end
+
+  def count_bytesize(file_text)
+    file_text.bytesize
   end
 end
 
