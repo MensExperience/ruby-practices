f929f0dfdbc8ec55781a884a519d535e1d35ad7f
shugowork <schw4one1win@gmail.com>
2021-09-14 22:43:38 +0900

    Add keyword argument.
    About #display_detailed_info

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -15,7 +15,7 @@ class Main
   def display_normal_input(opt)
     file_text = $stdin.read
     line_count, word_count, bytesize_count = count_file_info(file_text)
-    display_detailed_info(line_count, word_count, bytesize_count, opt, nil)
+    display_detailed_info(line_count: line_count, word_count: word_count, bytesize_count: bytesize_count, opt: opt)
   end
 
   def display_filepath_input(file_paths, opt)
@@ -25,15 +25,15 @@ class Main
     file_paths.each do |file_path|
       file_text = File.read(file_path)
       line_count, word_count, bytesize_count = count_file_info(file_text)
-      display_detailed_info(line_count, word_count, bytesize_count, opt, file_path)
+      display_detailed_info(line_count: line_count, word_count: word_count, bytesize_count: bytesize_count, opt: opt, file_path: file_path)
       total_line += line_count
       total_word += word_count
       total_bytesize += bytesize_count
     end
-    display_detailed_info(total_line, total_word, total_bytesize, opt, 'total') if file_paths.size > 1
+    display_detailed_info(line_count: total_line, word_count: total_word, bytesize_count: total_bytesize, opt: opt, file_path: 'total') if file_paths.size > 1
   end
 
-  def display_detailed_info(line_count, word_count, bytesize_count, opt, file_path)
+  def display_detailed_info(line_count:, word_count:, bytesize_count:, opt:, file_path: nil)
     print " #{format_value(line_count)}"
     unless opt['l']
       print " #{format_value(word_count)}"
