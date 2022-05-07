c414e15d4cbdf278331e8ce4fff4df3fa3821ffa
ShuGo <schw4one1win@gmail.com>
2022-05-07 17:35:50 +0900

    Fix each_with_index logic Major refactoring Implementation of the decision at the index.

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,38 +1,29 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-
-score_text = ARGV[0]
-scores = score_text.split(',')
-
+text_scores = ARGV[0].split(',')
 frame = []
-scores.each do |score|
-  if score == 'X'
+text_scores.each do |text_score|
+  if text_score == 'X'
     frame.push(10, 0)
   else
-    frame.push score.to_i
+    frame.push text_score.to_i
   end
 end
 
-game_score = frame.each_slice(2).to_a
-if game_score.size == 12
-  game_score[9].pop
-  game_score[9].push(game_score[10].first).push(game_score[11].first)
-  game_score.pop(2)
-elsif game_score.size == 11
-  game_score[9].push(game_score[10].first)
-  game_score.pop
-end
-
-total_point = game_score.each_with_index.sum do |frame, i|
-  if i <= 7 && frame.first == 10 && game_score[i + 1].first == 10
-    20 + game_score[i + 2].first
-  elsif i <= 8 && frame.first == 10
-    10 + game_score[i.next][0..1].sum
-  elsif i != 9 && frame.sum == 10
-    10 + game_score[i.next].first
+frames = frame.each_slice(2).to_a
+total_score =
+ frames.each_with_index.sum do |frame, i|
+  if i + 1 >= 10 #last_game
+    frame.sum
+  elsif frame.first == 10 && frames[i.next].first == 10 #double_strike
+    20 + frames[i + 2].first
+  elsif frame.first == 10 #single_strike
+    10 + frames[i.next][0..1].sum
+  elsif frame.sum == 10 #spare
+    10 + frames[i.next].first
   else
     frame.sum
   end
 end
 
-p total_point
+p total_score
