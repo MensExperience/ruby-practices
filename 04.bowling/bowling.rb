ee6c08ce51ad6b2a721732eff7e930cec1644b14
ShuGo <schw4one1win@gmail.com>
2022-05-07 17:44:44 +0900

    Implement strike and spare Modified conditional expressions to make them easier to read.

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -13,13 +13,15 @@ end
 frames = frame.each_slice(2).to_a
 total_score =
  frames.each_with_index.sum do |frame, i|
+  strike = (frame.first == 10)
+  spare =(!strike && frame.sum == 10)
   if i + 1 >= 10 #last_game
     frame.sum
-  elsif frame.first == 10 && frames[i.next].first == 10 #double_strike
+  elsif strike && frames[i.next].first == 10 #double_strike
     20 + frames[i + 2].first
   elsif frame.first == 10 #single_strike
     10 + frames[i.next][0..1].sum
-  elsif frame.sum == 10 #spare
+  elsif spare
     10 + frames[i.next].first
   else
     frame.sum
