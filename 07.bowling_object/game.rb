b29aa4d3b0860d2a25c4789765165ea7fecd17c1
ShuGo <schw4one1win@gmail.com>
2022-05-17 23:00:16 +0900

    Implement add_bonus and test Not oop

--- 07.bowling_object/game.rb
+++ 07.bowling_object/game.rb
@@ -2,24 +2,40 @@ require_relative 'frame'
 
 class Game
   def initialize(pinfall_text)
-    @frames = format_pinfalls(pinfall_text)
+    @game_score = 0
+    @pinfalls = format_pinfalls(pinfall_text)
   end
 
   def display_game_score
-    @frames
+    p calc_score(@pinfalls)
   end
 
   private
 
   def format_pinfalls(pinfall_text)
     pinfalls = pinfall_text.split(',').map { |c| c == 'X' ? 10 : c.to_i }
+  end
+
+  def calc_score(pinfalls)
     frames = []
-    pinfalls.each_with_index do |pinfall, index|
-      frames << [] if next_frame?(frames)
-      rolls = frames.last
-      rolls << pinfall
+    @game_score =
+      pinfalls.each_with_index.sum do |pinfall, index|
+        frames << [] if next_frame?(frames)
+        rolls = frames.last
+        rolls << pinfall
+        following_pinfalls = pinfalls[index.succ..]
+        last_frame?(frames) ? pinfall : pinfall + add_bonus(rolls, following_pinfalls)
+      end
+  end
+
+  def add_bonus(rolls, following_pinfalls)
+    if strike?(rolls)
+      following_pinfalls.first(2).sum
+    elsif spare?(rolls)
+      following_pinfalls.first
+    else
+      0
     end
-    frames
   end
 
   def last_frame?(frames)
@@ -30,10 +46,14 @@ class Game
     rolls[0] == 10
   end
 
+  def spare?(rolls)
+    rolls.sum == 10
+  end
+
   def next_frame?(frames)
     rolls = frames.last
     !last_frame?(frames) && (frames.empty? || strike?(rolls) || rolls.size == 2)
   end
 end
 
-p Game.new(ARGV[0]).display_game_score
+Game.new(ARGV[0]).display_game_score
---
+++ 07.bowling_object/test/test_poo_bowling.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require '../game'
+
+class GameTest < Minitest::Test
+  def test_all_strike
+    assert_equal 300, Game.new('X,X,X,X,X,X,X,X,X,X,X,X').display_game_score
+  end
+
+  def test_1
+    assert_equal 144, Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,1,8').display_game_score
+  end
+
+  def test_2
+    assert_equal 134, Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,0,0').display_game_score
+  end
+
+  def test_3
+    assert_equal 107, Game.new('0,10,1,5,0,0,0,0,X,X,X,5,1,8,1,0,4').display_game_score
+  end
+
+  def test_4
+    assert_equal 164, Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,X,X,X').display_game_score
+  end
+
+  def test_5
+    assert_equal 139, Game.new('6,3,9,0,0,3,8,2,7,3,X,9,1,8,0,X,6,4,5').display_game_score
+  end
+end
